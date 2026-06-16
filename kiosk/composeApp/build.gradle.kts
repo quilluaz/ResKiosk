44f9ee7a29d729d842c23c6279239d7894d92704
Isaac <quiloisaac@gmail.com>
2026-06-17 04:56:40 +0800

    fix task conflicts and settings repositories constraints for WasmJS

--- kiosk/composeApp/build.gradle.kts
+++ kiosk/composeApp/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
+    id("com.android.library")
     id("org.jetbrains.kotlin.multiplatform")
     id("org.jetbrains.compose")
     id("org.jetbrains.kotlin.plugin.compose")
-    id("com.android.library")
 }
 
 kotlin {
