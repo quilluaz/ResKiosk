ba5fbe5ce15837bd4a57c844c249c720750d97de
Isaac <quiloisaac@gmail.com>
2026-06-16 18:15:14 +0800

    change build base path to root for Vercel deployment

--- console/vite.config.js
+++ console/vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/console/', // Serve under /console prefix
+    base: '/',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
